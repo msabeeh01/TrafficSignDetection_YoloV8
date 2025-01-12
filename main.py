31db2dd8a4ec911a1c9e871b3a5ca7aa29145d01
Muhammad Sabeeh <muhammadsabeeh01@gmail.com>
2025-01-12 12:56:02 -0500

    inital commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import random
+import pandas as pd
+from PIL import Image
+import cv2
+from ultralytics import YOLO
+from IPython.display import Video
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style='darkgrid')
+import pathlib
+import glob
